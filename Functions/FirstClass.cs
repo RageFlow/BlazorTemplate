991d9347a8dcf810a43ce1265f98c9a7b06262f6
Julian Greger <juliangreger@msn.com>
2021-11-25 10:21:51 +0100

    Add project files.

---
+++ Functions/FirstClass.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorTemplate.Functions
+{
+    public class FirstClass
+    {
+        public FirstClass()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public string GetHello()
+        {
+            return "Hello";
+        }
+    }
+}
